f332dcbbba622b841b26ce6afa3780dc2310abbc
McTechie <mc.soans02@gmail.com>
2023-03-11 11:39:33 +0530

    feat: ability to download data

--- client/src/components/dashboard/ListingTable.tsx
+++ client/src/components/dashboard/ListingTable.tsx
@@ -1,6 +1,8 @@
 // named imports
 import { useRouter } from 'next/router'
-import { ethers } from 'ethers'
+
+// default imports
+import CsvDownloadButton from 'react-json-to-csv'
 
 interface ListingTableProps {
   appointments: Appointment[]
@@ -95,7 +97,7 @@ const ListingTable = ({ appointments, userDataLoading }: ListingTableProps) => {
 
     const MATIC_TO_MUBAI = amount / 83.05
     const FINAL_AMT = 1000000000000000000 * MATIC_TO_MUBAI
-    
+
     console.log(sender, receiver, FINAL_AMT)
 
     const params = [
@@ -105,7 +107,7 @@ const ListingTable = ({ appointments, userDataLoading }: ListingTableProps) => {
         value: Number(FINAL_AMT).toString(16)
       }
     ]
-    
+
     try {
       const result = await window.ethereum?.request({
         method: 'eth_sendTransaction',
@@ -119,75 +121,83 @@ const ListingTable = ({ appointments, userDataLoading }: ListingTableProps) => {
   }
 
   return (
-    <table className='w-full table-fixed text-center mt-4'>
-      <thead>
-        <tr className='table-header-row'>
-          <th className='hidden lg:block'>
-            Healthcare Provider
-          </th>
-          <th>
-            Doctor
-          </th>
-          <th className='hidden lg:block'>
-            Ailment
-          </th>
-          <th>
-            Date
-          </th>
-          <th className='hidden lg:block'>
-            Time
-          </th>
-          <th>
-            {' '}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {/* in case of appointments data available */}
-        {appointments?.map((appointment, index) => (
-          <tr
-            key={appointment.patientId + '_' + index}
-            className='table-body-row'
-            onClick={() => router.push(`/appointments/${appointment.patientId}`)}
-          >
-            <td className='hidden lg:block'>
-              {appointment.providerId.substring(0, 6)}.....{appointment.providerId.substring(appointment.providerId.length - 6, appointment.providerId.length)}
-            </td>
-            <td>
-              {appointment.doctor}
-            </td>
-            <td className='hidden lg:block'>
-              {appointment.ailment}
-            </td>
-            <td>
-              {appointment.date}
-            </td>
-            <td className='hidden lg:block'>
-              {appointment.time}
-            </td>
-            <td>
-              <button
-                onClick={(e) => handlePayment(e, appointment.patientId, appointment.providerId, appointment.consultationFee)}
-                className='text-white text-sm bg-emerald-600 py-[0.65rem] px-4 mx-2 rounded-full hover:shadow-sm shadow-emerald-300 hover:bg-emerald-700 cursor-pointer'
-              >
-                Pay Now
-              </button>
-            </td>
+    <div>
+      {appointments?.length > 0 && (
+        <div className='flex items-center justify-end mt-4'>
+          <div className='text-white bg-indigo-600 py-[0.65rem] px-4 mx-2 rounded-full hover:shadow-sm shadow-indigo-300 hover:bg-indigo-700 cursor-pointer'>
+            <CsvDownloadButton data={appointments} />
+          </div>
+        </div>
+      )}
+      <table className='w-full table-fixed text-center mt-8'>
+        <thead>
+          <tr className='table-header-row'>
+            <th className='hidden lg:block'>
+              Provider ID
+            </th>
+            <th>
+              Doctor
+            </th>
+            <th className='hidden lg:block'>
+              Ailment
+            </th>
+            <th>
+              Date
+            </th>
+            <th className='hidden lg:block'>
+              Time
+            </th>
+            <th>
+              Pay Provider
+            </th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {/* in case of appointments data available */}
+          {appointments?.map((appointment, index) => (
+            <tr
+              key={appointment.patientId + '_' + index}
+              className='table-body-row'
+            >
+              <td className='hidden lg:inline-block mt-2'>
+                {appointment.providerId.substring(0, 6)}.....{appointment.providerId.substring(appointment.providerId.length - 6, appointment.providerId.length)}
+              </td>
+              <td>
+                {appointment.doctor}
+              </td>
+              <td className='hidden lg:inline-block mt-2'>
+                {appointment.ailment}
+              </td>
+              <td>
+                {appointment.date}
+              </td>
+              <td className='hidden lg:inline-block mt-2'>
+                {appointment.time}
+              </td>
+              <td>
+                <button
+                  onClick={(e) => handlePayment(e, appointment.patientId, appointment.providerId, appointment.consultationFee)}
+                  className='text-white text-sm bg-emerald-600 py-[0.65rem] px-4 mx-2 rounded-full hover:shadow-sm shadow-emerald-300 hover:bg-emerald-700 cursor-pointer'
+                >
+                  Pay Now
+                </button>
+              </td>
+            </tr>
+          ))}
 
-        {/* in case of no data */}
-        {appointments?.length === 0 && (
-          <tr>
-            <td colSpan={5}>
-              <p className='text-center text-gray-500 mt-4'>
-                {userDataLoading ? 'Loading...' : 'No appointments found...'}
-              </p>
-            </td>
-          </tr>
-        )}
-      </tbody>
-    </table>
+          {/* in case of no data */}
+          {appointments?.length === 0 && (
+            <tr>
+              <td colSpan={5}>
+                <p className='text-center text-gray-500 mt-4'>
+                  {userDataLoading ? 'Loading...' : 'No appointments found...'}
+                </p>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
